bcda4438701d755b1a088c38993475d7998936b2
Gil Roman <groman@8thlight.com>
2018-09-20 15:24:41 -0700

    Add update_normal method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -11,6 +11,8 @@ class GildedRose
         update_sulfuras(item)
       when "Aged Brie"
         update_aged_brie(item)
+      when "normal"
+        update_normal(item)
       else
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
@@ -70,6 +72,16 @@ class GildedRose
       item.quality = item.quality + 1
     end
     item.quality = 50 if item.quality > 50
+    item.sell_in = item.sell_in - 1
+  end
+
+  def update_normal(item)
+    if item.sell_in < 0
+      item.quality = item.quality - 2
+    else
+      item.quality = item.quality - 1 
+    end
+    item.quality = 0 if item.quality < 0
     item.sell_in = item.sell_in - 1
   end
 end
