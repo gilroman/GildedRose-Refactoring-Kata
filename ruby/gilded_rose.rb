0f5f0e43f14c04c5249f70dd31ba37fdaea337ee
Gil Roman <groman@8thlight.com>
2018-09-20 15:33:53 -0700

    Add update_backstage_passes method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -7,12 +7,14 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       case item.name
-      when "Sulfuras, Hand of Ragnaros"
-        update_sulfuras(item)
       when "Aged Brie"
         update_aged_brie(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        update_backstage_passes(item)
       when "normal"
         update_normal(item)
+      when "Sulfuras, Hand of Ragnaros"
+        update_sulfuras(item)
       else
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
@@ -61,9 +63,6 @@ class GildedRose
 
     end
   end
-
-  def update_sulfuras(item)
-  end
   
   def update_aged_brie(item)
     if item.sell_in < 0
@@ -72,6 +71,14 @@ class GildedRose
       item.quality = item.quality + 1
     end
     item.quality = 50 if item.quality > 50
+    item.sell_in = item.sell_in - 1
+  end
+
+  def update_backstage_passes(item)
+    item.quality = item.quality + 1 if item.sell_in > 10
+    item.quality = item.quality + 2 if item.sell_in > 5 && item.sell_in <= 10
+    item.quality = item.quality + 3 if item.sell_in < 6
+    item.quality = 0 if item.sell_in < 1
     item.sell_in = item.sell_in - 1
   end
 
@@ -84,6 +91,10 @@ class GildedRose
     item.quality = 0 if item.quality < 0
     item.sell_in = item.sell_in - 1
   end
+
+  def update_sulfuras(item)
+  end
+
 end
 
 class Item
