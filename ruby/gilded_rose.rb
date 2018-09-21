2708acd7b9a4948bb06aa86c7411fb8a58e31a0b
Gil Roman <groman@8thlight.com>
2018-09-20 17:01:46 -0700

    Refactor GildedRose

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,43 +1,30 @@
 class GildedRose
 
   def initialize(items)
-    @items = items
+    @items = items.map do | item | 
+     getClassForItem(item).new(item)
+    end
+  end
+
+  def getClassForItem(item)
+    case item.name
+    when "Aged Brie"
+      Brie
+    when "Backstage passes to a TAFKAL80ETC concert"
+      BackstagePasses
+    when "Sulfuras, Hand of Ragnaros"
+      Sulfuras
+    else
+      Normal
+    end
   end
 
   def update_quality()
     @items.each do |item|
-      case item.name
-      when "Aged Brie"
-        return update_aged_brie(item)
-      when "Backstage passes to a TAFKAL80ETC concert"
-        update_backstage_passes(item)
-      when "normal"
-        update_normal(item)
-      when "Sulfuras, Hand of Ragnaros"
-        update_sulfuras(item)
-      end
-    end
+      item.update_quality
+    end  
   end
   
-  def update_aged_brie(item)
-    @item = Brie.new(item)
-    @item.update_quality 
-  end
-
-  def update_backstage_passes(item)
-    @item = BackstagePasses.new(item)
-    @item.update_quality
-  end
-
-  def update_normal(item)
-    @item = Normal.new(item)
-    @item.update_quality
-  end
-
-  def update_sulfuras(item)
-    @item = Sulfuras.new(item)
-    @item.update_quality
-  end
 end
 
 class BackstagePasses
